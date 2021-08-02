9cb2770a32f34286bb6ba18a90b32f5923993d9b
Tiemen <Tiemen2@live.nl>
2021-08-02 02:29:56 +0200

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# discord-datamining-action
-GitHub Action for posting commit comments in the Discord-Datamining repo
+# comments-action
+
+GitHub Action for posting commit comments in the Discord-Datamining repo.
+
+## Setup
+`.github/workflows/comments.yml`
+
+```yml
+name: Commit comments
+on:
+  push:
+    branches:
+      - master
+
+jobs:
+  comments:
+    name: Commit comments
+    runs-on: ubuntu-latest
+    steps:
+      - name: Comment on commit
+        uses: Discord-Datamining/comments-action@main
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import * as core from "@actions/core"
+import { context, getOctokit } from "@actions/github"
+import differ from "@adryd325/discord-datamining-lang-differ"
+import type { PushEvent } from "@octokit/webhooks-types"
+
+const token = process.env.GITHUB_TOKEN
+const filePathRegex = /\/\d{4}\/(?:\d{4}-\d{2}-\d{2}|\d{2}\/\d{2})\/[a-z0-9]{20,}\.js$/
+const currentFilename = "current.js"
+
+async function run() {
+    try {
+        if (!token) return core.setFailed("Invalid GITHUB_TOKEN")
+
+        const octokit = getOctokit(token)
+        const { owner, repo } = context.repo
+
+        if (context.eventName !== "push") return
+
+        const payload = context.payload as PushEvent
+        const commitSha = payload.after
+
+        const commit = await octokit.rest.repos.getCommit({
+            owner,
+            repo,
+            ref: commitSha
+        })
+
+        if (!commit)
+            return core.setFailed("commit not found")
+
+        const commitFile = commit.data.files?.[0]
+
+        if (!commitFile || commitFile?.status !== "added")
+            return core.info("not a build commit")
+
+        const { blob_url, sha: fileSha } = commit?.data?.files?.[0]
+
+        if (!filePathRegex?.test(blob_url))
+            return core.info("not a build file")
+
+        const currentTree = await octokit.rest.git.getTree({
+            owner,
+            repo,
+            tree_sha: payload.before,
+        })
+        const currentFileSha = currentTree?.data?.tree?.find?.(file => file.path === currentFilename)?.sha
+
+        if (!currentFileSha)
+            return core.info("no current file")
+
+        const currentFile = await octokit.rest.git.getBlob({
+            owner,
+            repo,
+            file_sha: currentFileSha
+        })
+        const newFile = await octokit.rest.git.getBlob({
+            owner,
+            repo,
+            file_sha: fileSha,
+        })
+
+        const currentContent = Buffer.from(currentFile.data.content, "base64").toString("utf8")
+        const newContent = Buffer.from(newFile.data.content, "base64").toString("utf8")
+        if (core.isDebug()) {
+            core.debug(`${currentContent.length}`)
+            core.debug(`${newContent.length}`)
+        }
+
+        let diff: string
+        try {
+            diff = differ(
+                currentContent,
+                newContent,
+                "codeblock",
+            )
+        } catch (e) {
+            return core.setFailed("unable to diff strings")
+        }
+
+        if (!diff)
+            return core.info("no strings changed")
+
+        await octokit.rest.repos.createCommitComment({
+            owner,
+            repo,
+            commit_sha: commitSha,
+            body: diff
+        })
+        return core.info("created commit comment")
+    } catch (error) {
+        core.setFailed(core.isDebug() ? error.stack : error.message)
+    }
+}
+run()
